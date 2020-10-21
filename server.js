d258928fe22af9ca595fd7500c3d488adb5c415f
Anar Enhsaihan <aenhsaihan@gmail.com>
2020-10-21 15:58:48 -0400

    Pull port from heroku env

--- server.js
+++ server.js
@@ -73,6 +73,6 @@ app.get('/logout', function(req, res) {
   res.redirect('/');
 });
 
-const server = app.listen(3000, function() {
+const server = app.listen(process.env.PORT || 5000, function() {
     console.log('Listening on port %d', server.address().port);
   });
