cdbfd65e4d50c4618e0dc8070f72484b446b7554
Anar Enhsaihan <aenhsaihan@gmail.com>
2020-10-22 02:54:53 -0400

    Use own id and access token

--- server.js
+++ server.js
@@ -75,7 +75,8 @@ app.get('/', function(req, res) {
 
 app.get('/matches', function(req, response) {
     if(req.isAuthenticated()) {
-        request('https://us.api.blizzard.com/sc2/legacy/profile/1/1/1024475/matches?access_token=USxkDM2RvhFks6p7Ff2G3IkfgI5Jy46L2i', { json: true }, (err, res, body) => {
+        const url = 'https://us.api.blizzard.com/sc2/legacy/profile/1/1/' + req.user.id + '/matches?access_token=' + req.user.token;
+        request(url, { json: true }, (err, res, body) => {
             if (err) { return console.log(err); }
 
             let output = "<table border='1|1'>";
@@ -91,10 +92,6 @@ app.get('/matches', function(req, response) {
                 output+="</tr>";
             }
             output+="</table>";
-
-            output += req.user.id;
-            output += req.user.token;
-
 
             response.send(output);
         });        
