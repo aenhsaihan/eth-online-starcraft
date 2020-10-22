366788fab9d9d54536247c7a7c6734eb7fca5e4b
Anar Enhsaihan <aenhsaihan@gmail.com>
2020-10-22 01:09:25 -0400

    Show the map of the first match

--- server.js
+++ server.js
@@ -57,10 +57,11 @@ app.get('/auth/bnet/callback',
 app.get('/', function(req, res) {
   if(req.isAuthenticated()) {
     var output = '<h1>Express OAuth Test</h1>' + req.user.id + '<br>';
+    const matches = req.user.matches;
+    output += matches[0].map;
     if(req.user.battletag) {
       output += req.user.battletag + '<br>';
     }
-    output += req.user;
     output += '<a href="/logout">Logout</a>';
     res.send(output);
   } else {
