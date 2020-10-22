2aec33009fec7146dfed9c0051c59299070afdf9
Anar Enhsaihan <aenhsaihan@gmail.com>
2020-10-22 00:57:19 -0400

    Log out all user details

--- server.js
+++ server.js
@@ -57,6 +57,7 @@ app.get('/auth/bnet/callback',
 app.get('/', function(req, res) {
   if(req.isAuthenticated()) {
     var output = '<h1>Express OAuth Test</h1>' + req.user.id + '<br>';
+    console.log(req.user);
     if(req.user.battletag) {
       output += req.user.battletag + '<br>';
     }
